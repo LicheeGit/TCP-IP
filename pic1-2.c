2a221af88b421b2e2288694dde22b22126fd94d4
LicheeGit <licheego@gmail.com>
2018-02-25 21:42:14 +0800

    包括了输出至标准错误的各个出错函数。

--- pic1-2.c
+++ pic1-2.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "my_error.h"
 
 #define BUFFSIZE 150 /* arbitrary size */
 int main()
@@ -22,7 +23,7 @@ int main()
 
     bzero((char *) &serv, sizeof(serv));
     serv.sin_family = AF_INET;
-    serv.sin_addr.s_addr = inet_addr("140.252.1.32");
+    serv.sin_addr.s_addr = inet_addr("203.107.6.88");   //这里的IP地址无法确定
     serv.sin_port = htons(13);
 
     if(sendto(sockfd, buff, BUFFSIZE, 0,
